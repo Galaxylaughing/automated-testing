8cb57bfeb5516a0fd14b6fd762e50129179c7268
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-26 08:56:12 -0700

    implemented initialize and make_a_deck methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,15 +4,27 @@
 require_relative 'card'
 
 class Deck
-
+  
+  attr_reader :card_list
+  
   def initialize
-
+    @card_list = []
+    make_a_deck()
   end
-
+  
+  def make_a_deck()
+    Card::VALUES.each do |value|
+      Card::SUITS.each do |suit|
+        new_card = Card.new(value, suit)
+        card_list.push(new_card)
+      end
+    end
+  end
+  
   def draw
     # returns a card
   end
-
+  
   def shuffle
     # shuffles the deck
   end
